5cd4d2ab0aabedb642b96a34101c79dfb00dc655
aziz279 <73511815+aziz279@users.noreply.github.com>
2022-06-08 19:30:11 +0700

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+	<title>FORM SIGN UP</title>
+</head>
+<body>
+
+	<form method="POST" action="simpansignup.php" >
+
+		<div align="center">
+	 	FORM SIGN UP
+	 </div>
+	 <div > Nama
+	 	<input name="nama">
+	 </div>
+	 <br>
+	 
+	 <div> Address
+	 	<input name="address">
+	 </div>
+	 <br>
+
+	 <div> Email
+	 	<input name="email">
+	 </div>
+	 <div>
+	 	<br>
+
+	 <div> Angkatan
+	 	<input name="angkatan">
+	 </div>
+	 <br>
+
+	 <div> Jurusan
+	 	<input name="jurusan">
+	 </div>
+	 <br>
+
+	 <div> Username
+	 	<input name="username">
+	 </div>
+	 <br>
+
+	 <div> Password
+	 	<input name="password">
+	 </div>
+	 <br>
+
+	 	<input type="submit" name="simpan">
+	 	<input type="reset" name="Reset">
+	 </div>
+
+
+	</form>
+	 
+	 
+</body>
+</html>
