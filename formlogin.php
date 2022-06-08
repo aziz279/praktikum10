4eeffc8ed92c3997e6be9173902d20702f763584
aziz279 <73511815+aziz279@users.noreply.github.com>
2022-06-08 19:31:21 +0700

    Create formlogin.php

---
+++ formlogin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>FORM LOGIN</title>
+</head>
+<body>
+	<form method="POST" action="simpanlogin.php">
+		
+		<div align="center">
+			FORM SIGN UP
+		</div>
+	<div> Username
+		<input name="username">
+	</div>
+	<br>
+
+	<div> Password
+		<input name="password">
+	</div>
+	<br>
+
+	<input type="submit" name="simpan" value="Login">
+	<input type="reset" name="Reset">
+
+	</form>
+</body>
+</html>
